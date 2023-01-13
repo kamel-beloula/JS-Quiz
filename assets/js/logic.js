da44f7c2492e8aff5138e6784e45a886b8697893
kamel beloula <kamel.solo@live.com>
2023-01-13 22:22:18 +0000

    added class hide to start screen js code

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,7 +1,8 @@
-var secondsLeft = 100;
+var secondsLeft = 10;
 const startButton = document.getElementById('start');
 const timer = document.getElementById('time');
-
+const startScreen = document.getElementById('start-screen');
+const questionsScreen = document.getElementById('questions');
 
 
 
@@ -11,11 +12,15 @@ startButton.addEventListener('click', startQuiz);
 
 function startQuiz(){
     startTimer();
+    startScreen.classList.add('hide');
+    questionsScreen.classList.remove('hide');
 }
 
 function startTimer(){
     var timeInterval = setInterval(() => {
     secondsLeft--
     timer.textContent = secondsLeft;
+    if (secondsLeft === 0){
+        clearInterval(timeInterval);}
     }, 1000);
 }
