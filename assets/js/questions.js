4e50d95256680120ee428314a38d53580dde0166
kamel beloula <kamel.solo@live.com>
2023-01-13 21:21:06 +0000

    added array of objects with questions and options to the questions js file

---
+++ assets/js/questions.js
@@ -0,0 +1,42 @@
+// create an array of objects with questions, options and correct answers 
+let  qb =[
+    
+{question: "Commonly used data types DO Not Include",
+    a: "1. strings", 
+    b: "2. booleans",
+    c: "3. alerts",
+    d: "4. numbers",
+correct: "3. alerts"},
+
+{question: "The condition in an if / else statement is enclosed with",
+
+        a: "1. quotes",
+        b: "2. curly brackets",
+        c: "3. parenthesis",
+        d: "4. square brackets",
+    correct: "3. parenthesis"},
+
+{question: "Arrays in JavaScript can be used to store?",
+
+    a: "1. number and strings", 
+    b: "2. other arrays",
+    c: "3. booleans",
+    d: "4. all of the above",
+correct: "4. all of the above" },
+
+{question: "String values must be enclosed within __________ when being assigned to variables.",
+
+    a: "1. commas", 
+    b: "2. curly brackets",
+    c: "3. quotes",
+    d: "4. parenthesis",
+correct: "3. quotes" },
+
+{question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+
+    a: "1. JavaScript", 
+    b: "2. terminal/bash",
+    c: "3. for loops",
+    d: "4. console.log",
+correct: "4. console.log" },
+]
