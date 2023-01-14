a085ed2adee4d67e82972771fad5441a3fe6216d
kamel beloula <kamel.solo@live.com>
2023-01-14 11:02:44 +0000

    added function to show question and choices -not working-

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,10 +1,12 @@
-var secondsLeft = 10;
 const startButton = document.getElementById('start');
 const timer = document.getElementById('time');
 const startScreen = document.getElementById('start-screen');
 const questionsScreen = document.getElementById('questions');
-
-
+const questionTitle = document.getElementById('question-title');
+const questionChoices = document.getElementById('choices');
+let currentQuiz = 0
+let score = 0
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startQuiz);
 
@@ -14,8 +16,12 @@ function startQuiz(){
     startTimer();
     startScreen.classList.add('hide');
     questionsScreen.classList.remove('hide');
+    showQuestion();
+    shuffledQuestions = questions.sort(() => Math.random() - .5);
+    currentQuestionIndex = 0;
 }
 
+var secondsLeft = 100;
 function startTimer(){
     var timeInterval = setInterval(() => {
     secondsLeft--
@@ -25,16 +31,66 @@ function startTimer(){
     }, 1000);
 }
 
-function showQuestion(questions){
-    questionElement.innerText = questions.question;
-    questions.answers.forEach(answer => {
-        const button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add('btn')
-        if (answer.correct){
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener('click',selectAnswer)
-        answerButtonsElement.appendChild(button);
-    });
+function setNextQuestion(){
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+} 
+
+function showQuestion(question){
+    questionTitle.innerText = question.question
+    
 }
+
+
+const  questions = [
+{
+    question: "Commonly used data types DO Not Include",
+    answers: [
+        {text: "1. strings", correct: false },
+        {text: "2. booleans", correct: true},
+        {text: "3. alerts", correct: false},
+        {text: "4. numbers", correct: false}
+    ]
+},
+
+{
+    question: "The condition in an if / else statement is enclosed with",
+    answers: [
+        {text: "1. quotes", correct: false },
+        {text: "2. curly brackets", correct: false},
+        {text: "3. parenthesis", correct: true},
+        {text: "4. square brackets", correct: false}
+    ]
+},    
+
+{
+    question: "Arrays in JavaScript can be used to store?",
+    answers: [
+        {text: "1. number and strings", correct: false },
+        {text: "2. other arrays", correct: false},
+        {text: "3. booleans", correct: false},
+        {text: "4. all of the above", correct: true}
+    ]
+},
+
+{
+    question: "String values must be enclosed within __________ when being assigned to variables.",
+    answers: [
+        {text: "1. commas", correct: false },
+        {text: "2. curly brackets", correct: false},
+        {text: "3. quotes", correct: true},
+        {text: "4. parenthesis", correct: false}
+    ]
+},
+
+{
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    answers: [
+        {text: "1. JavaScript", correct: false },
+        {text: "2. terminal/bash", correct: false},
+        {text: "3. for loops", correct: false},
+        {text: "4. console.log", correct: true}
+    ]
+}
+]
+
+    
